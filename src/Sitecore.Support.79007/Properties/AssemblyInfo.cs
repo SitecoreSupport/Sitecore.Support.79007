15378d24d4476859173a8f5562331217a927d9db
Alexander Lebedenko <ale@sitecore.net>
2017-02-07 15:26:56 +0200

    Patch for issue 79007

---
+++ src/Sitecore.Support.79007/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.79007")]
+[assembly: AssemblyProduct("Sitecore.Support.79007")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.79007/SitecoreAuthorizeAttribute.cs
@@ -0,0 +1,41 @@
+﻿namespace Sitecore.Support.EmailCampaign.Server.Filters
+{
+    using Sitecore.Diagnostics;
+    using Sitecore.Security.Accounts;
+    using System;
+    using System.Web.Http;
+    using System.Web.Http.Controllers;
+
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+    internal sealed class SitecoreAuthorizeAttribute : AuthorizeAttribute
+    {
+        private static readonly ITicketManager TicketManager = new TicketManagerWrapper();
+
+        public SitecoreAuthorizeAttribute(params string[] roles)
+        {
+            base.Roles = string.Join(",", roles);
+        }
+
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            Assert.ArgumentNotNull(actionContext, "actionContext");
+            bool flag = base.IsAuthorized(actionContext) && !this.AdminsOnly;
+            User principal = actionContext.ControllerContext.RequestContext.Principal as User;
+            bool flag2 = (principal != null) && principal.IsAdministrator;
+            return ((flag || flag2) && TicketManager.IsCurrentTicketValid());
+        }
+
+        public bool AdminsOnly { get; set; }
+
+        internal interface ITicketManager
+        {
+            bool IsCurrentTicketValid();
+        }
+
+        private class TicketManagerWrapper : SitecoreAuthorizeAttribute.ITicketManager
+        {
+            public bool IsCurrentTicketValid() =>
+                TicketManager.IsCurrentTicketValid();
+        }
+    }
+}
